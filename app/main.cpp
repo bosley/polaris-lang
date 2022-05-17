f9a5894bdb65bfa6847e3624ecfe2df1f6964690
bosley <bosley117@gmail.com>
2022-05-16 20:45:01 -0400

    Made a 'feeder' and comment system

--- app/main.cpp
+++ app/main.cpp
@@ -7,6 +7,69 @@
 #include <sstream>
 #include <vector>
 
+//! \brief A simple feeder object that makes sure that 
+//!        messy user input is massaged into something
+//!        nice and formal that can be accepted into 
+//!        the system
+class feeder_c {
+public:
+  //! \brief Create the feeder
+  //! \param evaluator The evaluator to use
+  //! \param env The environment to use in execution
+  feeder_c(polaris::evaluator_c &evaluator, 
+    std::shared_ptr<polaris::environment_c> env) 
+    : _eval(evaluator), _env(env) {}
+
+  //! \brief Feed the line into the system. 
+  //! \returns true iff a statement was submitted
+  bool feed(std::string &line, bool print_result=false) {
+
+    // Remove comments
+    std::size_t comment_loc = line.find_first_of(";");
+    if (comment_loc != std::string::npos) {
+      line = line.substr(0, comment_loc);
+    }
+
+    // If the line is empty pretend that we sent something
+    // so if REPL is active then the primpt will show again
+    if (line.empty()) {
+      return true;
+    }
+
+    // Walk the line and see if the given parens
+    // indicate that we have a full 
+    for(auto &c : line) {
+      if (c == '(') { _tracker++; }
+      else if (c == ')') { _tracker--;}
+      _statement += c;
+    }
+
+    // If we have a statement and all parens are closed then 
+    // we can submit the statement
+    if (_tracker == 0 && !_statement.empty()) {
+      auto result = _eval.evaluate(polaris::read(_statement), _env);
+
+      // If they requested that we print the result,
+      // then print the result
+      if (print_result) {
+        std::cout << polaris::to_string(result) << std::endl;
+      }
+      _statement.clear();
+      return true;
+
+    } else {
+      _statement += ' ';
+    }
+    return false;
+  }
+
+private:
+  polaris::evaluator_c &_eval;
+  std::shared_ptr<polaris::environment_c> _env;
+  uint64_t _tracker{0};
+  std::string _statement;
+};
+
 void help() {
   std::cout << "\nHelp : " << std::endl
             << "-i | --include  < ':' delim list >    Add include directories\n"
@@ -25,36 +88,15 @@ void version() {
 void repl(const std::string &prompt, polaris::evaluator_c &evaluator,
           std::shared_ptr<polaris::environment_c> env) {
 
-  std::string command;
-  uint64_t tracker{0};
+  bool show_prompt{true};
+  feeder_c feeder(evaluator, env);
   while(1) {
-    if (tracker == 0) { 
-      std::cout << prompt;
+    if (show_prompt) {
+      std::cout << "polaris> ";
     }
-
     std::string line;
     std::getline(std::cin, line);
-    if(line.empty()) {
-      continue;
-    }
-    for(auto &c : line) {
-      if (c == '(') { tracker++; }
-      else if (c == ')') { tracker--;}
-      command += c;
-    }
-
-    std::cout << "Command : " << command << std::endl;
-
-    if (tracker == 0 && !command.empty()) {
-      std::cout << polaris::to_string(
-                      evaluator.evaluate(polaris::read(command), env))
-                << std::endl;
-      command.clear();
-    } else {
-
-      // In case they hit return - we need to space out inputs
-      command += ' ';
-    }
+    show_prompt = feeder.feed(line);
   }
 }
 
@@ -76,25 +118,9 @@ void execute(const std::string &file, polaris::evaluator_c &evaluator,
   }
 
   std::string line;
-  std::string command;
-  uint64_t tracker {0};
+  feeder_c feeder(evaluator, env);
   while (std::getline(fs, line)) {
-    if(line.empty()) {
-      continue;
-    }
-
-    for(auto &c : line) {
-      if (c == '(') { tracker++; }
-      else if (c == ')') { tracker--;}
-      command += c;
-    }
-
-    if (tracker == 0 && !command.empty()) {
-      evaluator.evaluate(polaris::read(command), env);
-      command.clear();
-    }else {
-      command += ' ';
-    }
+    feeder.feed(line);
   }
 }
 
